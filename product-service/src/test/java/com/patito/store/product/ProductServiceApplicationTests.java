b6883a938789820b083b39e3234dde45aaf0e508
Erick Quintanar <quintanar777@gmail.com>
2023-12-01 12:45:36 -0600

    First commit, add product service

---
+++ product-service/src/test/java/com/patito/store/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patito.store.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
